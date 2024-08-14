469eb5de88018fcecf2b8444e8c5415c694bcd0b
herreang000 <herreang000@warren.k12.in.us>
2024-08-14 11:45:00 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class assignment {
+    public static void main(String[] args) {
+
+    public static int checkNumber(int number){
+        if(number > 0){
+           System.out.println("Positive");
+        } else if(number < 0){
+           System.out.println("Negative");
+        } else if(number == 0){
+           System.out.println("Zero");
+        }else{
+           System.out.println("Error");
+        }
+    }
+}
