28d1f2a2aee8d2b51b6f5c5a929394d08ff6ec1a
herreang000 <herreang000@warren.k12.in.us>
2024-08-14 11:45:19 -0400

    f

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-public class assignment {
+public class Main {
     public static void main(String[] args) {
 
-    public static int checkNumber(int number){
-        if(number > 0){
-           System.out.println("Positive");
-        } else if(number < 0){
-           System.out.println("Negative");
-        } else if(number == 0){
-           System.out.println("Zero");
-        }else{
-           System.out.println("Error");
+
+        public static int checkNumber(int number) {
+            if (number > 0) {
+                System.out.println("Positive");
+            } else if (number < 0) {
+                System.out.println("Negative");
+            } else {
+                System.out.println("Zero");
+            }
         }
+
+
+
     }
-}
+
+    }
+
